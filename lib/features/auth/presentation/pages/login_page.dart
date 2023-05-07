165f7e8d2907314865039446184fda448ac18bbc
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-05-07 15:16:15 +0700

    complete progress slicing ui

---
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:smart_parking/core.dart';
+import 'package:smart_parking/features/auth/presentation/pages/register_page.dart';
+import 'package:smart_parking/features/auth/presentation/widgets/divider_text.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    phoneController.clear();
+    passwordController.clear();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: AppSizes.secondary, vertical: 50.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                50.0.height,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Image.asset(AppImages.logo),
+                ),
+                40.0.height,
+                SmartFormInput(
+                  controller: phoneController,
+                  hint: "Nomor Telepon",
+                  icon: AppIcons.phone,
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Nomor telepon masih kosong";
+                    } else if (value.isValidPhoneNumber()) {
+                      return "Nomor Telepon tidak valid";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                20.0.height,
+                SmartFormInput(
+                  controller: passwordController,
+                  hint: "Kata Sandi",
+                  icon: AppIcons.password,
+                  isPassword: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Kata sandi masih kosong";
+                    } else if (value.length < 6) {
+                      return "Kata sandi terdiri dari minimal 6 karakter";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                40.0.height,
+                SmartFormButton(
+                  text: "Masuk",
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      "Yeay! Login berhasil".succeedBar(context);
+                    }
+                  },
+                ),
+                30.0.height,
+                const DividerText(text: "atau"),
+                30.0.height,
+                SmartFormOutlinedButton(
+                  text: "Buat Akun Baru",
+                  onPressed: () => context.push(const RegisterPage()),
+                ),
+                50.0.height,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/widgets/divider_text.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:smart_parking/core.dart';
+
+class DividerText extends StatelessWidget {
+  final String text;
+  final double thickness;
+  final Color color;
+
+  const DividerText({
+    super.key,
+    required this.text,
+    this.thickness = 1,
+    this.color = AppColors.primary,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(
+            thickness: thickness,
+            color: color,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: color,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            thickness: thickness,
+            color: color,
+          ),
+        ),
+      ],
+    );
+  }
+}
